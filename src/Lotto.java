14b0446760710118f2aa408453ada09800339fb1
kenu <kenu.heo@gmail.com>
2012-07-26 11:55:47 +0900

    getLotto()

--- src/Lotto.java
+++ src/Lotto.java
@@ -8,13 +8,15 @@ public class Lotto {
 	 * 인스턴스 변수
 	 */
 	public static void main(String[] args) {
-		new Lotto().printLotto();
+		new Lotto().getLotto();
 	}
 	
-	public void printLotto() {
+	public int[] getLotto() {
+		int[] numbers = new int[6];
 		for (int i = 0; i < 6; i++) {
-			System.out.println(getNumber());
+			numbers[i] = getNumber();
 		}
+		return numbers;
 	}
 
 	Random random = new Random();
